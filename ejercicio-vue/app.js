e7403191d0d6cfca91fdeb68739e91848c92ed17
omiras <oscar.miras@gmail.com>
2022-09-12 12:39:41 +0200

    first commit

---
+++ ejercicio-vue/app.js
@@ -0,0 +1,54 @@
+Vue.createApp({
+    data() {
+        return {
+            todos: [],
+            addTaskName: '',
+            hideCompleted: false,
+            textToFilter: ''
+        }
+    },
+
+    methods: {
+        addTodo() {
+
+            console.log("Add: ", this.addTaskName)
+
+            // Actualizamos el estado
+            const id = uuidv4();
+
+            this.todos.push({
+                id,
+                text: this.addTaskName,
+                done: false
+            });
+
+            this.addTaskName = "";
+
+        },
+
+        removeItem(id) {
+            console.log(id);
+            this.todos = this.todos.filter(todo => todo.id != id);
+        },
+
+        toggleCompleted(id) {
+            console.log("complete: ", id)
+
+            this.todos = this.todos.map(todo => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+        },
+
+        mustHideTask(done, text) {
+            return (done && this.hideCompleted) || !text.includes(this.textToFilter);
+        }
+
+    }
+}).mount('#app')
