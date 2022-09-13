48190dde9a9cbb03a5a706a857276c49e36fb6c7
omiras <oscar.miras@gmail.com>
2022-09-13 12:20:52 +0200

    array

---
+++ array-methods/array-fun.js
@@ -0,0 +1,12 @@
+/**
+ * Se ha creado una nueva unidad para medir el veneno de las arañas. Se llama 
+ * venominia, que es la cantidad de toxinas por mm de sangre que una araña
+ * puede inyectar. 
+ * 
+ * Se pide implementar una función que dada una entrada, filtre, usando el método
+ * filter, todas las arañas que al menos tienen dicho nivel de venominia.
+ */
+ function filterByVenom(spiders, venominia) {
+    return spiders.filter()
+}
+
---
+++ array-methods/tests/array-fun.test.js
@@ -0,0 +1,48 @@
+import { describe, expect, it } from "vitest";
+
+describe('filter method', () => {
+
+    it('should return all the spiders that have at least 50 units of venominia', () => {
+        const spiders = [{
+            name: "Tarantula Goliat",
+            venominia: 70
+        }, {
+            name: "Wolf Spider",
+            venominia: 50
+        }, {
+            name: "Black Vidow",
+            venominia: 44
+        }];
+
+        const result = filterByVenom(spiders, 50);
+
+        expect(result).toEqual([{
+            name: "Tarantula Goliat",
+            venominia: 70
+        }, {
+            name: "Wolf Spider",
+            venominia: 50
+        }]);
+
+    });
+
+    it('should return an empty array when no spiders meets the venominia criteria', () => {
+        const spiders = [{
+            name: "Tarantula Goliat",
+            venominia: 70
+        }, {
+            name: "Wolf Spider",
+            venominia: 50
+        }, {
+            name: "Black Vidow",
+            venominia: 44
+        }];
+
+        const result = filterByVenom(spiders, 100);
+
+        expect(result).toEqual([]);
+
+    })
+
+
+})
