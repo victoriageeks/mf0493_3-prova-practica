eb96c51256f1d6b571f607aeb56c0bf7a8fbe0df
Victoria Geeks <victoriageeks@gmail.com>
2022-09-15 15:27:44 +0200

    primer commit prueba práctica

--- array-methods/array-fun.js
+++ array-methods/array-fun.js
@@ -7,7 +7,7 @@
  * filter, todas las arañas que al menos tienen dicho nivel de venominia.
  */
  function filterByVenom(spiders, venominia) {
-    return spiders.filter()
+    return spiders.filter(x=>x>=venominia)
 }
 
 module.exports = {
