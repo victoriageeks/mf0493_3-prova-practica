0468077dee7ff19aab269a3fd168ac2da36743e2
Victoria Geeks <victoriageeks@gmail.com>
2022-09-15 16:26:22 +0200

    unit-testing

--- unit-testing/isogram.test.js
+++ unit-testing/isogram.test.js
@@ -1,3 +1,17 @@
 import {it, expect, describe} from 'vitest';
 import { isIsogram } from './isogram';
 
+it('should return true because there is no repeated letter', () => {
+    const result = isIsogram("Dermatoglyphics");
+    expect(result).toBe(true);
+});
+
+it('should return false because there are repeated letters', () => {
+    const result = isIsogram("aba");
+    expect(result).toBe(false);
+});
+
+it('should return false because ignore letter case', () => {
+    const result = isIsogram("moOse");
+    expect(result).toBe(false);
+});
